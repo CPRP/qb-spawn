9a7044ed635c6c3a422f01305b026e46a287a0f0
Jordan <jordanbscott12@gmail.com>
2022-04-28 15:40:48 -0400

    Added more spawn locations

--- config.lua
+++ config.lua
@@ -10,18 +10,54 @@ QB.Spawns = {
     ["policedp"] = {
         coords = vector4(428.23, -984.28, 29.76, 3.5),
         location = "policedp",
-        label = "Police Department",
+        label = "Los Santos PD",
     },
 
     ["paleto"] = {
         coords = vector4(80.35, 6424.12, 31.67, 45.5),
         location = "paleto",
-        label = "Paleto Bay",
+        label = "Paleto Bay Garage",
     },
 
-    ["motel"] = {
-        coords = vector4(327.56, -205.08, 53.08, 163.5),
-        location = "motel",
-        label = "Motels",
+    ["cityservices"] = {
+        coords = vector4(-260.18, -973.69, 31.22, 27.42),
+        location = "cityservices",
+        label = "City Services",
+    },
+
+    ["impound"] = {
+        coords = vector4(403.31, -1626.83, 29.29, 249.99),
+        location = "impound",
+        label = "Impound Lot",
+    },
+
+    ["pillbox"] = {
+        coords = vector4(296.46, -601.93, 43.3, 81.57),
+        location = "pillbox",
+        label = "Pillbox Hospital",
+    },
+
+    ["sandygarage"] = {
+        coords = vector4(1737.37, 3710.74, 34.13, 47.27),
+        location = "sandygarage",
+        label = "Sandy Shores Garage",
+    },
+
+    ["paletopd"] = {
+        coords = vector4(-448.3, 6025.87, 31.49, 347.44),
+        location = "paletopd",
+        label = "Paleto Bay PD",
+    },
+
+    ["trucking"] = {
+        coords = vector4(157.16, -3210.79, 7.03, 332.43),
+        location = "trucking",
+        label = "Walker Logistics Trucking",
+    },
+
+    ["taxi"] = {
+        coords = vector4(896.12, -178.24, 74.7, 303.71),
+        location = "taxi",
+        label = "Downtown Cab",
     },
 }
